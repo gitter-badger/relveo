e0bc765b110a9544a406fb59348a011414c3edc7
Andy Costanza <costalfy@gmail.com>
2017-03-24 17:10:14 +0100

    Initial commit

---
+++ src/main/java/com/andycostanza/relveo/RelveoApplication.java
@@ -0,0 +1,12 @@
+package com.andycostanza.relveo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelveoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelveoApplication.class, args);
+	}
+}
