fac7483a57a1b8f519295e82d6036db1fea21fde
Andy Costanza <andy.costanza@ucm.be>
2017-04-28 09:07:42 +0200

    upgrade to angular 4.1.0

---
+++ frontend/src/main/frontend/updateAngular.bat
@@ -0,0 +1,1 @@
+npm install @angular/common@latest @angular/compiler@latest @angular/compiler-cli@latest @angular/core@latest @angular/forms@latest @angular/http@latest @angular/platform-browser@latest @angular/platform-browser-dynamic@latest @angular/platform-server@latest @angular/router@latest @angular/animations@latest typescript@latest --save
---
+++ frontend/src/main/frontend/updateAngular.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npm install @angular/{common,compiler,compiler-cli,core,forms,http,platform-browser,platform-browser-dynamic,platform-server,router,animations}@latest typescript@latest --save
