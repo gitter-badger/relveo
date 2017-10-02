9acc72ba86f61040339720836030e839ff17777f
Andy Costanza <costalfy@gmail.com>
2017-10-02 14:13:55 +0200

    upgrade angular

--- frontend/src/main/frontend/updateAngular.sh
+++ frontend/src/main/frontend/updateAngular.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npm install @angular/{common,compiler,compiler-cli,core,forms,http,platform-browser,platform-browser-dynamic,platform-server,router,animations}@latest typescript@latest --save
+npm install @angular/{cli,common,compiler,compiler-cli,core,forms,http,platform-browser,platform-browser-dynamic,platform-server,router,animations}@latest typescript@latest --save
