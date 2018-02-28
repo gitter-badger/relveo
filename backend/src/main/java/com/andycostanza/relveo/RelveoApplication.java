577d1ee722a5e2fd035835aae0822ecddca1b45c
Andy Costanza <costalfy@gmail.com>
2018-02-28 12:24:33 +0100

    Launch browser automatically after backend is ready

--- backend/src/main/java/com/andycostanza/relveo/RelveoApplication.java
+++ backend/src/main/java/com/andycostanza/relveo/RelveoApplication.java
@@ -1,12 +1,45 @@
 package com.andycostanza.relveo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
+@Slf4j
 public class RelveoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RelveoApplication.class, args);
+	}
+
+	@EventListener({ApplicationReadyEvent.class})
+	void applicationReadyEvent() {
+		log.info("Relveo is started ... launching browser now");
+		Browse("http://localhost:8080");
+	}
+
+	public static void Browse(String url) {
+		if(Desktop.isDesktopSupported()){
+			Desktop desktop = Desktop.getDesktop();
+			try {
+				desktop.browse(new URI(url));
+			} catch (IOException | URISyntaxException e) {
+				log.error(e.getMessage(),e);
+			}
+		}else{
+			Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+			} catch (IOException e) {
+                log.error(e.getMessage(),e);
+			}
+		}
 	}
 }
